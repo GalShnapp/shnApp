7f2681c9cb039328c8139e206ab8ca3ddef80766
Gal Shnapp <galshnapp@Gals-Mac-mini.local>
2022-07-22 16:20:20 +0300

    some revisions, initial thing is going on

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
+import uvicorn
 from fastapi import FastAPI
-from db.model import Parent, Child
-from db.engine import get_sandbox_db_session
+from db.model import Parent, Child, Association
+from db.engine import get_sandbox_db_transaction
 
 app = FastAPI()
 
@@ -8,3 +9,44 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/get_children_ids/{parent_id}")
+async def get_children_ids(parrent_id: int):
+    _p = None
+    with get_sandbox_db_transaction() as transaction:
+        p = transaction.query(Parent).join(Child).filter(Parent.id == parrent_id).first()
+        _p = [child.id for child in p.children]
+    return {"message": f"Hello parent {parrent_id}, your children will be right here!\n enjoy {_p}"}
+
+@app.get("/get_parent_id/{child_id}")
+async def get_parent_id(child_id: int):
+    return {"message": f"Hello Child {child_id}, your parent will be right here!"}
+
+@app.post("/new_parent")
+async def create_new_parent():
+    p = Parent()
+    a = Association(extra_data="some data")
+    a.child = Child()
+    p.children.append(a)
+    pid = 0
+    with get_sandbox_db_transaction() as transaction:
+        transaction.add(p)
+        transaction.commit()
+        pid = p.id
+    return {"message": f"Hello new parent. Your parent id is {pid}"}
+
+@app.post("/new_parent_with_child")
+async def create_new_parent_with_child():
+    p = Parent()
+    a = Association(extra_data="some data")
+    a.child = Child()
+    p.children.append(a)
+    pid = 0
+    with get_sandbox_db_transaction() as transaction:
+        transaction.add(p)
+        transaction.commit()
+        pid = p.id
+    return {"message": f"Hello new parent. Your parent id is {pid}"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- db/engine.py
+++ db/engine.py
@@ -1,11 +1,36 @@
+from typing import Callable
+from contextlib import contextmanager
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from secret_store import SANDBOX_DB_PASSWORD, SANDBOX_DB_USERNAME, SANDBOX_DB_SCHEMA
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.engine.url import URL
+from typing import ContextManager
+from secret_store import SANDBOX_DB_PASSWORD, SANDBOX_DB_USERNAME, SANDBOX_DB_SCHEMA, SANDBOX_DB_HOST
 
-CONNECTION_STRING = f'postgresql://{SANDBOX_DB_USERNAME}:{SANDBOX_DB_PASSWORD}@localhost/{SANDBOX_DB_SCHEMA}?currentSchema={SANDBOX_DB_SCHEMA}'
+
+CONNECTION_STRING = str(URL('postgresql+psycopg2',
+                            username=SANDBOX_DB_USERNAME,
+                            password=SANDBOX_DB_PASSWORD,
+                            host=SANDBOX_DB_HOST,
+                            port=5432,
+                            database=SANDBOX_DB_SCHEMA
+                            ))
 
 def get_sandbox_db_engine():
-    return create_engine(CONNECTION_STRING)
+    return create_engine(CONNECTION_STRING, connect_args={
+        'options': f'-csearch_path={SANDBOX_DB_SCHEMA}'
+    })
 
-def get_sandbox_db_session():
-    return sessionmaker(get_sandbox_db_engine)
+def get_sandbox_db_session() -> Session:
+    return sessionmaker(bind=get_sandbox_db_engine())
+
+@contextmanager
+def get_sandbox_db_transaction() -> ContextManager[Session]:
+    session_maker = get_sandbox_db_session()
+    session = session_maker()
+    try:
+        yield session
+    except:
+        session.rollback()
+    finally:
+        session.close()
+    
