d4fe060faaa9e1ec2de3b00bd999a445f6cb559a
Gal Shnapp <gal.shnapp@gmail.com>
2022-07-24 17:50:32 +0300

    Feature/docker compose (#9)
    * small move around
    
    * refactors are epic
    
    * things are now packages
    
    * ok finally no more refactorios
    
    * added template docker-compose (#8)
    
    * dockerized app
    
    * composed isnt working
    
    * composed isnt working
    
    * compose works :)
    
    * touches
    
    Co-authored-by: Gal Shnapp <galshnapp@Gals-Mac-mini.local>
    Co-authored-by: Roybge <86953290+Roybge@users.noreply.github.com>

--- app/app/db_connectors/engine.py
+++ app/app/db_connectors/engine.py
@@ -4,8 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.engine.url import URL
 from typing import ContextManager
-from secret_store import SANDBOX_DB_PASSWORD, SANDBOX_DB_USERNAME, SANDBOX_DB_SCHEMA, SANDBOX_DB_HOST
+import os
 
+try:
+    from secret_store import SANDBOX_DB_USERNAME, SANDBOX_DB_PASSWORD, SANDBOX_DB_SCHEMA, SANDBOX_DB_HOST
+except:
+    SANDBOX_DB_USERNAME = os.environ.get("SANDBOX_DB_USERNAME", None)
+    SANDBOX_DB_PASSWORD = os.environ.get("SANDBOX_DB_PASSWORD", None)
+    SANDBOX_DB_SCHEMA = os.environ.get("SANDBOX_DB_SCHEMA", None)
+    SANDBOX_DB_HOST = os.environ.get("SANDBOX_DB_HOST", None)
 
 CONNECTION_STRING = str(URL('postgresql+psycopg2',
                             username=SANDBOX_DB_USERNAME,
@@ -29,7 +36,9 @@ def get_sandbox_db_transaction() -> ContextManager[Session]:
     session = session_maker()
     try:
         yield session
-    except:
+    except Exception as e:
+        print("exceptions suck")
+        print(e)
         session.rollback()
     finally:
         session.close()
--- db/README.md
+++ db/README.md
@@ -1,9 +1,1 @@
-## engine
-use this to connect to the project's DB
-
-## model
-use this to declate the DB's schema
-
-## alembic
-is a version control tool over the schema. 
-it reads the model and uses the engine to manage the db.
+## Env for DB
