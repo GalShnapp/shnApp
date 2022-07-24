0d8a80e1c099b65f550e3c0dba2381ee8e9328cd
roybargil <roybargil@gmail.com>
2022-07-25 01:25:01 +0300

    fixed running the script , not the alembic is stuck

--- app/script.sh
+++ app/script.sh
@@ -3,19 +3,15 @@ set -e
 echo start 
 pwd 
 ls -l 
-cd app/ 
+cd app/
+
 
 echo step2 
 pwd 
 ls -l 
-cd app/ 
-
-echo step3 
-pwd 
-ls -l 
 cd db_connectors/
 
-echo "and now we wait ....."
+echo "\n\n\nand now we wait .....\n\n\n"
 sleep 10 
 
 echo step-alembic  
