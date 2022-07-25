3363b7183434598fa166c088f94ed84f764fc83e
Gal Shnapp <galshnapp@Gals-Mac-mini.local>
2022-07-25 15:07:27 +0300

    cleaner

--- app/app/db_connectors/engine.py
+++ app/app/db_connectors/engine.py
@@ -21,10 +21,10 @@ CONNECTION_STRING = str(URL('postgresql+psycopg2',
                             port=5432,
                             database=SANDBOX_DB_SCHEMA
                             ))
-print(CONNECTION_STRING)
+
 def get_sandbox_db_engine():
     return create_engine(CONNECTION_STRING, connect_args={
-        'options': f'-csearch_path={SANDBOX_DB_SCHEMA}'
+        'options': f'-c search_path={SANDBOX_DB_SCHEMA}'
     })
 
 def get_sandbox_db_session() -> Session:
